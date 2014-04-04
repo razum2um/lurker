295d2f786607c332d7945480b1e1e7057977a82f
Vlad Bokov <razum2um@mail.ru>
2014-04-04 21:26:41 +0700

    increase aruma timeout

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,6 +2,7 @@ require 'aruba/cucumber'
 
 Before do
   @dirs = ["tmp/example_app"]
+  @aruba_timeout_seconds = 30
 end
 
 #ENV['PATH'] = "#{File.expand_path('../tmp/example_app', __FILE__)}#{File::PATH_SEPARATOR}#{ENV['PATH']}"
