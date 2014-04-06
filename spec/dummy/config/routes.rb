ce5de54cb1afa6b1697b4c689fe26635ffa66593
Vlad Bokov <razum2um@mail.ru>
2014-04-06 14:19:07 +0700

    remove dummy app

--- spec/dummy/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Dummy::Application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      resources :users, only: [:index, :create, :show, :destroy]
-    end
-  end
-end
--- spec/dummy/db/migrate/20140404072422_create_api_v1_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateApiV1Users < ActiveRecord::Migration
-  def change
-    create_table :users do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
