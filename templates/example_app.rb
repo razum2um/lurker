d381576c759ffadde8d8897231b9e7ca1354bff2
Vlad Bokov <razum2um@mail.ru>
2014-04-08 10:18:20 +0700

    use local copy of gem on travis

--- templates/example_app.rb
+++ templates/example_app.rb
@@ -1,5 +1,6 @@
 file '.bundle/config', <<-CODE
 ---
+BUNDLE_LOCAL__LURKER: #{File.expand_path '../..', __FILE__}
 BUNDLE_PATH: #{File.expand_path '../../vendor/bundle', __FILE__}
 CODE
 
@@ -8,5 +9,5 @@ gem 'spring-commands-rspec'
 gem 'simplecov', '~> 0.7.1', require: false
 gem 'pry-debugger', require: false
 gem 'pry-stack_explorer', require: false
-gem 'lurker', github: 'razum2um/lurker', branch: 'master', require: 'lurker/server'
+gem 'lurker', github: 'razum2um/lurker', branch: 'master'
 
