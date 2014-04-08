d67ba7b241009973e8a63e033720d4746af76527
Vlad Bokov <razum2um@mail.ru>
2014-04-08 11:33:27 +0700

    try to fix local branch

--- templates/example_app.rb
+++ templates/example_app.rb
@@ -9,5 +9,5 @@ gem 'spring-commands-rspec'
 gem 'simplecov', '~> 0.7.1', require: false
 gem 'pry-debugger', require: false
 gem 'pry-stack_explorer', require: false
-gem 'lurker', github: 'razum2um/lurker', branch: 'master'
+gem 'lurker', github: 'razum2um/lurker'
 
