eab536754b7c94496186e4eb8101069320bcb65d
Vlad Bokov <razum2um@mail.ru>
2014-04-08 11:46:19 +0700

    speedup travis

--- templates/example_app.rb
+++ templates/example_app.rb
@@ -1,6 +1,5 @@
 file '.bundle/config', <<-CODE
 ---
-# BUNDLE_LOCAL__LURKER: #{File.expand_path '../..', __FILE__}
 BUNDLE_PATH: #{File.expand_path '../../vendor/bundle', __FILE__}
 CODE
 
@@ -9,5 +8,5 @@ gem 'spring-commands-rspec'
 gem 'simplecov', '~> 0.7.1', require: false
 gem 'pry-debugger', require: false
 gem 'pry-stack_explorer', require: false
-gem 'lurker', github: 'razum2um/lurker', branch: 'master'
+gem 'lurker', github: 'razum2um/lurker', branch: 'master', path: "#{File.expand_path '../..', __FILE__}"
 
