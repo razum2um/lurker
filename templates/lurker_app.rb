571d00c62020520b67a7fa33a630b15b5f76e48d
Vlad Bokov <razum2um@mail.ru>
2014-04-24 06:29:04 +0700

    add unicorn to prod

--- templates/lurker_app.rb
+++ templates/lurker_app.rb
@@ -9,6 +9,7 @@ gem 'database_cleaner'
 gem 'simplecov', '~> 0.7.1', require: false
 gem 'pry-debugger', require: false
 gem 'pry-stack_explorer', require: false
+gem 'unicorn', group: :production
 
 append_to_file 'Gemfile' do
   <<-CODE
@@ -20,5 +21,36 @@ append_to_file 'Gemfile' do
       # run at heroku
       gem 'lurker', github: 'razum2um/lurker', branch: 'master'
     end
+  CODE
+end
+
+file 'config/unicorn.rb' do
+  <<-CODE
+    worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+    timeout 15
+    preload_app true
+
+    before_fork do |server, worker|
+      Signal.trap 'TERM' do
+        puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+        Process.kill 'QUIT', Process.pid
+      end
+
+      defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+    end
+
+    after_fork do |server, worker|
+      Signal.trap 'TERM' do
+        puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+      end
+
+      defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+    end
+  CODE
+end
+
+file 'Procfile' do
+  <<-CODE
+    web: ./bin/unicorn -p $PORT -c ./config/unicorn.rb
   CODE
 end
