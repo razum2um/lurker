b940fd294a0aaf932f9a90b9180e36d00e658bb9
Vlad Bokov <razum2um@mail.ru>
2014-04-30 14:00:00 +0700

    include engine only once

--- lib/lurker/engine.rb
+++ lib/lurker/engine.rb
@@ -3,8 +3,10 @@ require 'lurker/server'
 module Lurker
   class Engine < ::Rails::Engine
     config.after_initialize do |app|
-      app.routes.prepend do
-        mount Lurker::Server.to_rack(path: 'html'), at: "/#{Lurker::DEFAULT_URL_BASE}"
+      app.routes.append do
+        unless ::Rails.env.production? || app.routes.routes.map(&:app).any? { |e| e.is_a?(Class) && e.name == 'Lurker::Rack' }
+          mount Lurker::Server.to_rack(path: 'html'), at: "/#{Lurker::DEFAULT_URL_BASE}"
+        end
       end
     end
   end
