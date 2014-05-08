0fa9a120fc0ddfa7caee4a21b4f270deba13914f
Vlad Bokov <razum2um@mail.ru>
2014-05-08 16:21:10 +0700

    WTF??? fix coveralls overall coverage

--- templates/generate_stuff.rb
+++ templates/generate_stuff.rb
@@ -28,7 +28,9 @@ inject_into_file "bin/lurker", before: /require .lurker.$/ do
       SimpleCov.start do
         filters.clear # This will remove the :root_filter that comes via simplecov's defaults
         add_filter do |src|
-          !(src.filename =~ /^\#{SimpleCov.root}\\/lib\\/lurker/)
+          # WTF??? if erb_schema_context gets into .resultset.json second time
+          # coveralls WONT calculate overall coverage... o_O
+          src.filename =~ /erb_schema_context/ || !(src.filename =~ /^\#{SimpleCov.root}\\/lib\\/lurker/)
         end
       end
     end
