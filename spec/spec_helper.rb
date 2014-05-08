05f2ec8d93a2b755eab0b063ddad55299e37e8fd
Vlad Bokov <razum2um@mail.ru>
2014-05-08 23:07:37 +0700

    proper coverage from own rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,14 @@ require 'bundler/setup'
 require 'lurker'
 
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  filters.clear # This will remove the :root_filter that comes via simplecov's defaults
+  add_filter do |src|
+    # WTF??? if erb_schema_context gets into .resultset.json second time
+    # coveralls WONT calculate overall coverage... o_O
+    src.filename =~ /erb_schema_context/ || !(src.filename =~ /^#{SimpleCov.root}\/lib\/lurker/)
+  end
+end
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
