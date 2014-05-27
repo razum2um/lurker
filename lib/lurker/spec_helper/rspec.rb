64d190461e3c55a80fc9c92e8026cb5dd335fbb7
Vlad Bokov <razum2um@mail.ru>
2014-05-27 19:37:44 +0700

    get rid of legacy hack

--- lib/lurker/spec_helper/rspec.rb
+++ lib/lurker/spec_helper/rspec.rb
@@ -1,53 +1,7 @@
 require 'lurker/spy'
 
-module Lurker
-  module SpecHelper
-    module Rspec
-      extend ActiveSupport::Concern
-      ACTIONS = [:get, :post, :put, :delete, :patch].freeze
-
-      included do
-        ACTIONS.each do |verb|
-          send(:define_method, "#{verb}_with_lurker") do |*params|
-            action, request_params, env = params
-
-            request_params ||= {}
-            env ||= {}
-
-            # obsolete, support controller -> request migration
-            # supports `get :index` in request specs
-            if @_example && @_example.metadata[:type] == :request && action.is_a?(Symbol)
-              unless @_example.metadata.described_class.is_a?(Class)
-                raise 'cannot determine request url: provide proper described class like: "describe MyController do"'
-              end
-              controller_name = @_example.metadata.described_class.name.tableize.gsub(/_controllers$/, '')
-              action = URI.parse(url_for({ controller: controller_name, action: action }.merge(request_params))).path
-            end
-
-            send("#{verb}_without_lurker", action, request_params, env)
-          end
-
-          begin
-            send :alias_method_chain, verb, :lurker
-          rescue NameError
-            # no patch in Rails3.2
-            if verb == :patch
-              alias_method :patch_without_lurker, :put_without_lurker
-              alias_method :patch, :patch_with_lurker
-            else
-              raise
-            end
-          end
-        end
-      end
-    end
-  end
-end
-
 if defined?(RSpec) && RSpec.respond_to?(:configure)
   RSpec.configure do |config|
-    config.include Lurker::SpecHelper::Rspec, type: :controller
-    config.include Lurker::SpecHelper::Rspec, type: :request
 
     lurker = ->(example) {
       # RSpec::Core::ExampleGroup::Nested_1 === self
