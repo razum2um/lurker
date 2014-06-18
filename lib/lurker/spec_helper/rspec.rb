a50eeee8a96c1966377a5270fd509052b3410a52
Vlad Bokov <razum2um@mail.ru>
2014-06-18 11:29:24 +0700

    changed hook detection in rspec

--- lib/lurker/spec_helper/rspec.rb
+++ lib/lurker/spec_helper/rspec.rb
@@ -1,21 +1,21 @@
 require 'lurker/spy'
 
 if defined?(RSpec) && RSpec.respond_to?(:configure)
+  when_tagged_with_lurker = { :lurker => lambda { |v| !!v } }
+
   RSpec.configure do |config|
-
-    lurker = ->(example) {
-      # RSpec::Core::ExampleGroup::Nested_1 === self
-      @_example = example
-
-      if (metadata = example.metadata[:lurker]).present?
-        Lurker::Spy.on(suffix: metadata, &example)
+    config.around(:each, when_tagged_with_lurker) do |ex|
+      example = ex.respond_to?(:metadata) ? ex : ex.example
+      options = example.metadata[:lurker]
+      options = if options.is_a?(Hash)
+        options.dup
+      elsif options.is_a?(String)
+        { suffix: options }
       else
-        example.call
+        {}
       end
-    }
-
-    config.around(:each, type: :controller, &lurker)
-    config.around(:each, type: :request, &lurker)
+      Lurker::Spy.on(options, &example)
+    end
 
     unless RSpec::Core::Version::STRING > '3'
       config.treat_symbols_as_metadata_keys_with_true_values = true
