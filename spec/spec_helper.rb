9c1456eb4c4ff7c0aa47158be185f83c23ff5453
Vlad Bokov <vlad@lunatic.cat>
2020-11-16 09:57:05 +0300

    No deprecated rspec option

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,6 @@ end
 Dir[File.expand_path '../support/**/*.rb', __FILE__].each { |file| require file }
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.order = 'random'
